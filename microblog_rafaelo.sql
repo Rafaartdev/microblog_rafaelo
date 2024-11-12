bd61616fc6c19d0d8844f7d95559555d97d41504
Rafaartdev <silveira.orafa@gmail.com>
2024-11-12 17:23:38 -0300

    efetuado Back-up

---
+++ microblog_rafaelo.sql
@@ -0,0 +1,120 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 12/11/2024 às 21:19
+-- Versão do servidor: 10.4.32-MariaDB
+-- Versão do PHP: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `microblog_rafaelo`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `noticias`
+--
+
+CREATE TABLE `noticias` (
+  `id` int(11) NOT NULL,
+  `data` datetime NOT NULL DEFAULT current_timestamp(),
+  `titulo` varchar(150) NOT NULL,
+  `texto` text NOT NULL,
+  `resumo` text NOT NULL,
+  `imagem` varchar(45) NOT NULL,
+  `usuario_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `noticias`
+--
+
+INSERT INTO `noticias` (`id`, `data`, `titulo`, `texto`, `resumo`, `imagem`, `usuario_id`) VALUES
+(2, '2024-11-12 16:39:39', 'O poder do came came ha', 'Destroidor e vencedor em lutas', 'Baita golpe', 'batalha.jpg', 4),
+(3, '2024-11-12 16:40:24', 'De volta para o futuro', 'Um carro ao qual efetua em voiagens no tempo ', 'Delorian', 'ate1984.jpg', 2),
+(5, '2024-11-12 16:47:27', 'Os golfinhos possuem um time', 'Miami Dolphins é o time predileto e com boas ações paras a natureza e mares  ', 'Dolphins do bem', 'golfinhos.jpg', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `senha` varchar(255) NOT NULL,
+  `tipo` enum('admin','editor') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `nome`, `email`, `senha`, `tipo`) VALUES
+(1, 'Rafael', 'rafael.osilveira@gmail.com', '102030rafa', 'admin'),
+(2, 'Golfinho', 'golfinho@gmail.com', '102030gol', 'editor'),
+(3, 'OUTATIME', 'delorian@gmail.com', '102030del', 'editor'),
+(4, 'Goku', 'goku@gmail.com', 'dragon10', 'editor');
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `noticias`
+--
+ALTER TABLE `noticias`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_noticias_usuarios` (`usuario_id`);
+
+--
+-- Índices de tabela `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `noticias`
+--
+ALTER TABLE `noticias`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT de tabela `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Restrições para tabelas despejadas
+--
+
+--
+-- Restrições para tabelas `noticias`
+--
+ALTER TABLE `noticias`
+  ADD CONSTRAINT `fk_noticias_usuarios` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
