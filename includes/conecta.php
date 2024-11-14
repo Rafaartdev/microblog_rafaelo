ddab92960a4fec84e7e8f392f725fbbc73e888cd
Rafaartdev <silveira.orafa@gmail.com>
2024-11-14 14:54:17 -0300

    conecção ao banco

---
+++ includes/conecta.php
@@ -0,0 +1,19 @@
+<?php
+//Parametrs d acwssoao servidor
+$servidor = "localhost";
+$usuario ="root";
+$senha = "";
+$banco = "microblog_rafaelo";
+
+//Acessar a função de conexão ao servidor de BD
+$conexao = mysqli_connect($servidor, $usuario, $senha, $banco);
+
+//Definindo o charset UTF8 para conexão
+mysql_set_charset($conexao,"utf8");
+
+//Verificando conexão
+if($conexao === false){
+    die("Erro: ".mysqli_connect_error());
+} else {
+    echo "Beleza, segue o jogo...";
+}
