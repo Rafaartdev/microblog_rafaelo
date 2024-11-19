73e084084a73108361639efbbe555567912bcfef
Rafaartdev <silveira.orafa@gmail.com>
2024-11-19 14:34:49 -0300

    atualizando dados do usuario

--- admin/usuarios.php
+++ admin/usuarios.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 require "../includes/cabecalho-admin.php";
 require "../includes/funcoes-usuarios.php";
 
@@ -8,57 +8,56 @@ $listaDeUsuarios = listarUsuarios($conexao);
 
 <div class="row">
 	<article class="col-12 bg-white rounded shadow my-1 py-4">
-		
+
 		<h2 class="text-center">
-		Usuários <span class="badge bg-dark">X</span>
+			Usuários <span class="badge bg-dark">X</span>
 		</h2>
 
 		<p class="text-center mt-5">
 			<a class="btn btn-primary" href="usuario-insere.php">
-			<i class="bi bi-plus-circle"></i>	
-			Inserir novo usuário</a>
+				<i class="bi bi-plus-circle"></i>
+				Inserir novo usuário</a>
 		</p>
-				
+
 		<div class="table-responsive">
-		
+
 			<table class="table table-hover">
 				<thead class="table-light">
-					<tr>
-						<th>Nome</th>
-						<th>E-mail</th>
-						<th>Tipo</th>
-						<th class="text-center">Operações</th>
-					</tr>
+				<tr>
+					<th>Nome</th>
+					<th>E-mail</th>
+					<th>Tipo</th>
+					<th class="text-center">Operações</th>
+				</tr>
 				</thead>
 
 				<tbody>
-<?php foreach( $listaDeUsuarios as $usuario ) { ?>
-					<tr>
-						<td> <?=$usuario['nome']?> </td>
-						<td> E-mail... </td>
-						<td> Tipo... </td>
-						<td class="text-center">
-							<a class="btn btn-warning" 
-							href="usuario-atualiza.php">
-							<i class="bi bi-pencil"></i> Atualizar
-							</a>
-						
-							<a class="btn btn-danger excluir" 
-							href="usuario-exclui.php">
-							<i class="bi bi-trash"></i> Excluir
-							</a>
-						</td>
-					</tr>
-<?php } ?>
-				</tbody>                
+					<?php foreach ($listaDeUsuarios as $usuario) { ?>
+						<tr>
+							<td> <?= $usuario['nome'] ?> </td>
+							<td> <?= $usuario['email'] ?> </td>
+							<td> <?= $usuario['tipo'] ?></td>
+							<td class="text-center">
+								<a class="btn btn-warning"
+									href="usuario-atualiza.php">
+									<i class="bi bi-pencil"></i> Atualizar
+								</a>
+
+								<a class="btn btn-danger excluir"
+									href="usuario-exclui.php">
+									<i class="bi bi-trash"></i> Excluir
+								</a>
+							</td>
+						</tr>
+					<?php } ?>
+				</tbody>
 			</table>
-	</div>
-		
+		</div>
+
 	</article>
 </div>
 
 
-<?php 
+<?php
 require "../includes/rodape-admin.php";
 ?>
-
